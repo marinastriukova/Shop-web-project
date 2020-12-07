3816b84fd722c3d3d90d73a7042b40a6edb151cc
Пользователь <andreyz11288@gmail.com>
2020-12-08 00:46:04 +0200

    2

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -1,25 +1,44 @@
 
-import hendel from '../../templates/pagination.hbs'
-const cardName = document.querySelector('.pagination-name-card')
-const render = document.querySelector('.pagination-card-all')
+// import hendelLi from '../../templates/pagination//paginationLi.hbs'
+import hendelDiv from '../../templates/pagination/paginationDiv.hbs'
+const groupName = document.querySelector('.pagination-name-group')
+const renderGroup = document.querySelector('.pagination-card-all')
+const renderDiv = document.querySelector('.section-pagination')
+// console.log(renderGroup);
+const API_KEY = '19060894-87e054058a337546d07970d77'
+    function renDiv(params) {
+        const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&page=1&per_page=15&key=${API_KEY}`
+    fetch(URL).then(r => r.json()).then(e => { 
+       console.log(e);
+        renderCard(e.hits)       
+    })
+    function renderCard(resul, search) {
+    renderDiv.innerHTML = ""
+    const markup = hendelDiv(resul, search);
+    renderDiv.insertAdjacentHTML('beforeend', markup);
+}
+    }
+renDiv(API_KEY)
+function renLi(params) {
+    
+const search = 'nice'
 
 
-const search = 'nike'
+// groupName.textContent = search
 
-
-
-
-    const API_KEY = '19060894-87e054058a337546d07970d77'
+    // const API_KEY = '19060894-87e054058a337546d07970d77'
     const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=6&key=${API_KEY}`
     fetch(URL).then(r => r.json()).then(e => { 
-        console.log(e);
+       console.log(e);
         renderCard(e.hits)       
     })
 
 
 
 function renderCard(resul) {
-    render.innerHTML = ""
-    const markup = hendel(resul);
-    render.insertAdjacentHTML('beforeend', markup);
+    renderGroup.innerHTML = ""
+    const markup = hendelDiv(resul);
+    renderGroup.insertAdjacentHTML('beforeend', markup);
 }
+}
+renLi(API_KEY)
