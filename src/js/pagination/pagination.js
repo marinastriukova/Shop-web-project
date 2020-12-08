ca3fec9b97dace10e8472ee93fe32dd3f19cbd5b
Пользователь <andreyz11288@gmail.com>
2020-12-09 00:52:50 +0200

    3

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -1,44 +1,59 @@
 
 // import hendelLi from '../../templates/pagination//paginationLi.hbs'
 import hendelDiv from '../../templates/pagination/paginationDiv.hbs'
-const groupName = document.querySelector('.pagination-name-group')
-const renderGroup = document.querySelector('.pagination-card-all')
-const renderDiv = document.querySelector('.section-pagination')
+// const groupName = document.querySelector('.pagination-div')
+const btn = document.getElementsByClassName('pagination-catigories-btn')
+const renderDiv = document.querySelector('.pagination-div')
 // console.log(renderGroup);
-const API_KEY = '19060894-87e054058a337546d07970d77'
-    function renDiv(params) {
-        const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&page=1&per_page=15&key=${API_KEY}`
-    fetch(URL).then(r => r.json()).then(e => { 
-       console.log(e);
-        renderCard(e.hits)       
-    })
-    function renderCard(resul, search) {
-    renderDiv.innerHTML = ""
-    const markup = hendelDiv(resul, search);
-    renderDiv.insertAdjacentHTML('beforeend', markup);
-}
-    }
-renDiv(API_KEY)
-function renLi(params) {
+function renLi() {
+    let page = 1
+    btn[0].addEventListener('click', btn1)
+    btn[1].addEventListener('click', btn2)
+    btn[2].addEventListener('click', btn3)
+    const URL = "https://callboard-backend.herokuapp.com/call?page="
+        
     
-const search = 'nice'
-
-
-// groupName.textContent = search
-
-    // const API_KEY = '19060894-87e054058a337546d07970d77'
-    const URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=1&per_page=6&key=${API_KEY}`
-    fetch(URL).then(r => r.json()).then(e => { 
-       console.log(e);
-        renderCard(e.hits)       
+    
+    const options = {
+        headers: {
+            Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiI1ZmNmODc2ZDJkNTVkOTAwMTdhZTMzOTciLCJzaWQiOiI1ZmNmZjYwMjI4YjhhNzAwMTc5ODMyNTMiLCJpYXQiOjE2MDc0NjQ0NTAsImV4cCI6MTYwNzQ2ODA1MH0.hMvcJNhFoPUUiHHAPpAb-JUa8geUs7yjO65xur79XxI"
+        }
+    }
+    
+   const fetchRender = fetch(`${URL}${page}`, options).then(r=>r.json()).then(e => {renderDi(e)       
     })
+    
+    function btn1() {        
+        page = 1
+        if(btn[1].classList.contains('activ')){btn[1].classList.remove('activ')} else if( btn[2].classList.contains('activ')){btn[2].classList.remove('activ')}
+        btn[0].classList.add('activ')
+        fetch(`${URL}${page}`, options).then(r=>r.json()).then(e => {
+        renderDi(e);        
+    })
+        
+    }
+    function btn2() {        
+        page = 2
+        if(btn[0].classList.contains('activ')){btn[0].classList.remove('activ')} else if( btn[2].classList.contains('activ')){btn[2].classList.remove('activ')}
+        btn[1].classList.add('activ')
+        fetch(`${URL}${page}`, options).then(r=>r.json()).then(e => {
+        renderDi(e)       
+    })
+    }
+    function btn3() {        
+        page = 3
+        if(btn[0].classList.contains('activ')){btn[0].classList.remove('activ')} else if( btn[1].classList.contains('activ')){btn[1].classList.remove('activ')}
+        btn[2].classList.add('activ')
+        fetch(`${URL}${page}`, options).then(r=>r.json()).then(e => {
+        renderDi(e)       
+    })
+    }
 
 
-
-function renderCard(resul) {
-    renderGroup.innerHTML = ""
-    const markup = hendelDiv(resul);
-    renderGroup.insertAdjacentHTML('beforeend', markup);
+    function renderDi(resul) {    
+        renderDiv.innerHTML = ""
+        const markup = hendelDiv(resul);    
+        renderDiv.insertAdjacentHTML('afterbegin', markup);
+    }
 }
-}
-renLi(API_KEY)
+renLi()
