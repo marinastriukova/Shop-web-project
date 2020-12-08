f344a2889729e5ba29454d8895a50a56d5ae3a89
Marina Striukova <65074360+marinastriukova@users.noreply.github.com>
2020-12-08 23:17:18 +0200

    добавляет вестку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
- import "./sass/main.scss";
+import "./sass/main.scss";
+
+
+// registration open/close logic
+
+const refs = {
+    openBtnRegistrModal : document.querySelector('[data-modal-open]'),
+    closeBtnRegistrModal : document.querySelector('[data-modal-close]'),
+    modalRegistr : document.querySelector('[data-modal]'), 
+}
+
+refs.openBtnRegistrModal.addEventListener('click', toggleRegistrModal);
+refs.closeBtnRegistrModal.addEventListener('click', toggleRegistrModal);
+
+function toggleRegistrModal () {
+    refs.modalRegistr.classList.toggle('is-hidden')
+}
