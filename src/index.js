6637c66687b0f17ae739fee71f76c915937bd491
Marina Striukova <65074360+marinastriukova@users.noreply.github.com>
2020-12-11 21:12:17 +0200

    выбивает ошибки при регистрации

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import "./sass/main.scss";
+import "./js/header/menu";
 import "./js/registration";
+import "./images/sprite/symbol-defs.svg"
 
--- src/js/registration.js
+++ src/js/registration.js
@@ -1,79 +1,120 @@
 import axios from 'axios';
-
+import { alert } from '@pnotify/core';
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
 ///////// registration open/close logic //////////////
 
 const refs = {
-    openBtnRegistrModal: document.querySelector('[data-modal-open]'),
-    closeBtnRegistrModal: document.querySelector('[data-modal-close]'),
-    modalRegistr: document.querySelector('[data-modal]'),
+  openBtnRegistrModal: document.querySelector('[data-modal-open]'),
+  closeBtnRegistrModal: document.querySelector('[data-modal-close]'),
+  modalRegistr: document.querySelector('[data-modal]'),
 }
 
 function toggleRegisterModal() {
-    refs.modalRegistr.classList.toggle('is-hidden')
+  refs.modalRegistr.classList.toggle('is-hidden')
 }
 
 refs.openBtnRegistrModal.addEventListener('click', toggleRegisterModal);
 refs.closeBtnRegistrModal.addEventListener('click', toggleRegisterModal);
 
 
+
+
+  if (refs.modalRegistr.classList === 'is-hidden') {
+    document.onkeydown = function (e) {
+      if (e.key === 'Escape') {
+        toggleRegisterModal();
+      }
+    }
+  }
+
+
+
+////BUTTON/////
+const singUp = document.querySelector('.registration-button-entry');
 const registerBtn = document.querySelector('.registration-button');
-registerBtn.addEventListener('click', toggleRegisterModal);
+
+// registerBtn.addEventListener('click', toggleRegisterModal);
+// singUp.addEventListener('click', toggleRegisterModal);
 
 
-
-////////// registr ////////////////
+/////////////////////////
 
 const BASE_URL = 'https://callboard-backend.herokuapp.com';
 
-const registerUser = userData => {
-     const { email, password } = userData;
 
-     return axios.post(`${BASE_URL}/auth/register`, { email, password });
- }
+
 
 const registerFormRef = document.querySelector('.registration-form');
 
 
-////////
-
 
 const handleRegisterSubmit = event => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const { currentTarget: form } = event;
-    const formData = new FormData(form);
-    const body = {};
+  const { currentTarget: form } = event;
+  const formData = new FormData(form);
+  const body = {};
 
-    formData.forEach((value, key) => {
-        body[key] = value;
-    })
-    console.log(body);
-    
-    //register(body);
-    registerUser(body);
-    login(body);
+  formData.forEach((value, key) => {
+    body[key] = value;
+  })
+  // console.log(body);
+
+  if (event.target.classList.value === "registration-button") {
+    register(body);
+    // toggleRegisterModal();
+  } else
+    if (event.target.classList.value === "registration-button-entry") {
+      login(body);
+      // toggleRegisterModal();
+    }
+
+
 }
 
-registerFormRef.addEventListener('submit', handleRegisterSubmit)
+// registerFormRef.addEventListener('submit', handleRegisterSubmit)
+registerFormRef.addEventListener('click', handleRegisterSubmit)
+
+
+////// registr ///////
+
+async function register(log) {
+  const regOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(log)
+  };
+
+  try {
+    fetch("https://callboard-backend.herokuapp.com/auth/register", regOptions)
+      .then(response => response.json())
+      .then(result => {
+        // console.log(result)
+        if (result.message === `User with ${JSON.parse(regOptions.body).email} email already exists`) {
+          pushError(result.message)
+        } else { 
+          console.log(result) }
+      })
+  }
+  catch (error) { console.log('error', error) };
+}
 
 
 
-////// ПОСМАН ///////
+//////error//////
 
-// function register(log){
-// const regOptions = {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(log)
-// };
-
-
+function pushError(error) {
+  alert({
+    title: `${error}`,
+    delay: 1500
+  });
+}
 
 ///////// login///////////
-
-function login(log){
+async function login(log) {
   const requestOptions = {
     method: 'POST',
     headers: {
@@ -81,9 +122,24 @@ function login(log){
     },
     body: JSON.stringify(log)
   };
-  
-  fetch("https://callboard-backend.herokuapp.com/auth/login", requestOptions)
-    .then(response => response.json())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+
+  try {
+    await fetch("https://callboard-backend.herokuapp.com/auth/login", requestOptions)
+
+      .then(response => response.json())
+      .then(result => {
+        if (result.message === 'Password is wrong') {
+          pushError(result.message)
+        } else if (result.message === `User with ${JSON.parse(requestOptions.body).email} email doesn't exist`) {
+          pushError(result.message)}
+          else{
+          console.log(result)
+        }
+      })
   }
+  catch (error) {
+    console.log('error', error);
+  };
+}
+
+
