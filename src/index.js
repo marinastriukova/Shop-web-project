0e2c0f35d49fe65c2c2caa4da1cc8b1e00317f9b
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2020-12-11 09:50:33 +0200

    add fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './sass/main.scss';
 import './images/sprite/symbol-defs.svg';
-
 import './js/header.js/menu';
 import './js/header.js/categories-API';
 import './js/add-modal-logic/add-modal-validation';
