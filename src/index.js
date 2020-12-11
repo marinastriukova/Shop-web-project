8b5b8274526b3fcc91d85d48bd577da31b2e42ee
DenisSvinkovskij <1911.denis55@gmail.com>
2020-12-11 18:22:11 +0200

    add fix validation

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import './images/sprite/symbol-defs.svg';
 import './js/header.js/menu';
 import './js/header.js/categories-API';
 import './js/add-modal-logic/add-modal-validation';
-
+import './js/add-modal-logic/add-modal-open';
+import './js/add-modal-logic/add-modal-close';
